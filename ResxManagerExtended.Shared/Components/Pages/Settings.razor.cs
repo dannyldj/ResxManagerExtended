d50ef28bf2da41bcb91ffb9ca4d076335f690294
Dongju <dongju@dannyldj.me>
2024-08-07 15:08:37 +0000

    지역화 지원을 위한 리소스 및 Localizer 추가

--- ResxManagerExtended.Shared/Components/Pages/Settings.razor.cs
+++ ResxManagerExtended.Shared/Components/Pages/Settings.razor.cs
@@ -1,6 +1,8 @@
 using Fluxor;
 using Fluxor.Blazor.Web.Components;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using ResxManagerExtended.Shared.Properties;
 using ResxManagerExtended.Shared.Store.Settings;
 using ResxManagerExtended.Shared.Store.Settings.UseCase;
 
@@ -8,9 +10,11 @@ namespace ResxManagerExtended.Shared.Components.Pages;
 
 public partial class Settings : FluxorComponent
 {
-    [Inject] public required IDispatcher Dispatcher { get; set; }
-    
-    [Inject] public required IState<RootDirectoryState> DirectoryState { get; set; }
+    [Inject] public required IStringLocalizer<Resources> Loc { private get; init; }
+
+    [Inject] public required IDispatcher Dispatcher { private get; init; }
+
+    [Inject] public required IState<RootDirectoryState> DirectoryState { private get; init; }
 
     private void GetRootDirectory()
     {
