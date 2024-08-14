98371e922ca14ec9b67c397f785fb0b2d7782932
Dongju <dongju@dannyldj.me>
2024-08-14 06:54:21 +0000

    Bootstrapper.cs 수정

--- ResxManagerExtended.Shared/Extensions/Bootstrapper.cs
+++ ResxManagerExtended.Shared/Extensions/Bootstrapper.cs
@@ -1,25 +1,15 @@
-using Fluxor;
-using Fluxor.Blazor.Web.ReduxDevTools;
 using KristofferStrube.Blazor.FileSystemAccess;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.FluentUI.AspNetCore.Components;
-using ResxManagerExtended.Shared.Components;
 
 namespace ResxManagerExtended.Shared.Extensions;
 
 public static class Bootstrapper
 {
-    public static void ConfigureServices(this IServiceCollection collection, bool isDevelopment)
+    public static void ConfigureServices(this IServiceCollection collection)
     {
         collection.AddLocalization();
         collection.AddFluentUIComponents();
-        collection.AddFluxor(options =>
-        {
-            options.ScanAssemblies(typeof(Routes).Assembly).UseRouting();
-
-            if (isDevelopment)
-                options.UseReduxDevTools(middlewareOptions => { middlewareOptions.Name = "ResxManagerExtended"; });
-        });
 
         collection.AddFileSystemAccessServiceInProcess();
     }
--- ResxManagerExtended.Web/Program.cs
+++ ResxManagerExtended.Web/Program.cs
@@ -1,3 +1,5 @@
+using Fluxor;
+using Fluxor.Blazor.Web.ReduxDevTools;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ResxManagerExtended.Shared.Components;
@@ -8,6 +10,14 @@ builder.RootComponents.Add<Routes>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.ConfigureServices(builder.HostEnvironment.IsDevelopment());
+builder.Services.AddFluxor(options =>
+{
+    options.ScanAssemblies(typeof(Routes).Assembly).UseRouting();
+
+    if (builder.HostEnvironment.IsDevelopment())
+        options.UseReduxDevTools(middlewareOptions => { middlewareOptions.Name = "ResxManagerExtended"; });
+});
+
+builder.Services.ConfigureServices();
 
 await builder.Build().RunAsync();
