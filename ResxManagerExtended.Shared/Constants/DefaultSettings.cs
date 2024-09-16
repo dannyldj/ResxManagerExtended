e8dcacfdee1654ebbb50fe1b05c048089682d1b3
Dongju <dongju@dannyldj.me>
2024-09-16 15:12:25 +0900

    정규식 그룹 이름 추가

--- ResxManagerExtended.Shared/Constants/DefaultSettings.cs
+++ ResxManagerExtended.Shared/Constants/DefaultSettings.cs
@@ -4,7 +4,8 @@ namespace ResxManagerExtended.Shared.Constants;
 
 public static class DefaultSettings
 {
-    public const string DefaultResourceRegex = "^(.+?)\\.(.+?)?\\.?resx$";
+    public const string ResourceRegexName = "name", ResourceRegexCode = "code";
+    public const string DefaultResourceRegex = @$"^(?<{ResourceRegexName}>.+?)\.(?<{ResourceRegexCode}>.+?)?\.?resx$";
 
     public static readonly IEnumerable<CultureInfo> AvailableCultures = [new("en"), new("ko")];
 
