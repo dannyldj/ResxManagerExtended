3b01b3f3d6493ba080554346cca00eb8927bb182
Dongju <dongju@dannyldj.me>
2024-09-20 01:47:00 +0900

    웹 환경 경고 메시지 한 번만 표시되도록 수정

--- ResxManagerExtended.Shared/Components/Pages/Home.razor.cs
+++ ResxManagerExtended.Shared/Components/Pages/Home.razor.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
-using Microsoft.FluentUI.AspNetCore.Components;
-using ResxManagerExtended.Shared.Constants;
 using ResxManagerExtended.Shared.Properties;
 
 namespace ResxManagerExtended.Shared.Components.Pages;
@@ -9,23 +7,4 @@ namespace ResxManagerExtended.Shared.Components.Pages;
 public partial class Home
 {
     [Inject] public required IStringLocalizer<Resources> Loc { private get; init; }
-
-    [Inject] public required IMessageService MessageService { private get; init; }
-
-    protected override void OnInitialized()
-    {
-        if (OperatingSystem.IsBrowser() is false) return;
-
-        MessageService.ShowMessageBar(options =>
-        {
-            options.Section = MessageBarSection.SectionTop;
-            options.Body = Loc["NoticeWeb"];
-            options.Intent = MessageIntent.Warning;
-            options.Link = new ActionLink<Message>
-            {
-                Text = Loc["LearnMore"],
-                Href = "https://github.com/dannyldj/ResxManagerExtended/releases"
-            };
-        });
-    }
 }
